52c877bf7153abbffddd4496b881f3169a750f50
V3nus3 <cectirado@gmail.com>
2024-07-09 10:37:17 -0400

    FOR

---
+++ FOR/1_13_rewrite1_4_1.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+int main(){
+    //1.9
+    /*int sum=0;
+    for(int a =50;a<=100;++a){
+        sum+=a;
+
+    }
+    cout<<"the sum of the numbers from 50 to 100 is "<<sum<<endl;*/
+    //1.10 countdown
+    /*
+    for (int a=10;a>=0;--a){
+        cout<<a<<endl;
+    }*/
+   //1.11
+   int a,b;
+   cin>>a;
+   cin>>b;
+   for(int x=a+1;x<b;++x){
+    cout<<x<<endl;
+   }
+}
